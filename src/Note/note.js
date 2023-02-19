8ee2b7c4ce274b877c1863e60db4bd56df4e1f16
LIMJAEHUN <ijhgun007@naver.com>
2023-02-19 21:43:24 +0900

    5일차 object

--- src/Note/note.js
+++ src/Note/note.js
@@ -173,6 +173,42 @@ log(1234);
 const user1 = new User('Lim','jaehun',-1);
 console.log(user1.age);
 
+class Shape{
+    constructor(width, height, color){
+        this.width = width;
+        this.height= height;
+        this.color = color;
+    } // 필드정의
+
+    draw(){
+        console.log(`drawing ${this.color} color of`);
+    }
+
+    getArea() {
+        return this.width * this.height;
+    }
+
+    
+}
+class Rectangle extends Shape {}
+class Triangle extends Shape {
+
+    draw() {
+        super.draw();
+        console.log('▲');
+    }
+    getArea() {
+        return (this.width * this.height)/2;
+    }
+
+}
+
+const  rectangle = new Rectangle(20,20, 'blue');
+rectangle.draw();
+console.log(rectangle.getArea())
+const  triangle = new Triangle(20,20, 'blue');
+triangle.draw();
+console.log(triangle.getArea())
 
     return(
         <div> 연습장 용도</div>
